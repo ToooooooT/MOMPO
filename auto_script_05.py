7b6dbd84ef24f7468b78209d5cfac24928cc086d
SharpKoi <sharpkoi1222@gmail.com>
2023-06-11 15:05:03 +0800

    feat: add auto-script for running setting

---
+++ auto_script_05.py
@@ -0,0 +1,11 @@
+import os
+
+eps_time = .05
+ratio_set = [0.5, 1.0, 1.1, 1.2, 1.3,  1.4,  1.5]
+tolerance = [0.7, 0.7, 8.2, 8.2, 11.5, 15.1, 23.7]
+alpha = .01
+
+for ratio, tol in zip(ratio_set, tolerance):
+    print(f"Running setting ratio={ratio}, tol={tol} ...")
+    eps_treasure = ratio * eps_time
+    os.system(f"python run_cate_mompo.py --epsilons \"{eps_treasure},{eps_time}\" --tolerance {tol} --alpha {alpha}")
